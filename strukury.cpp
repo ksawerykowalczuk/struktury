2d0bdbb0465963b2f6ee07402b6372af48309146
ksawerykowalczuk <ksawery.kowalczuk@gmail.com>
2024-07-31 20:40:05 +0200

    koniec :)))))))))))))))))))))))))

--- strukury.cpp
+++ strukury.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 
+struct Data
+{
+    int rok;
+    int mies;
+    int dzien;
+};
+
 struct Osoba
 {
     string imie;
     string nazwisko;
-    int rok;
-    int mies;
-    int dzien;
+    Data dzienurodzenia;
 };
 
 Osoba wczytywanieosoby()
@@ -18,52 +23,54 @@ Osoba wczytywanieosoby()
     cout << "podaj nazwisko: ";
     cin >> osoba.nazwisko;
     cout << "podaj rok: ";
-    cin >> osoba.rok;
+    cin >>  osoba.dzienurodzenia.rok;
     cout << "podaj mies: ";
-    cin >> osoba.mies;
+    cin >> osoba.dzienurodzenia.mies;
     cout << "podaj dzien: ";
-    cin >> osoba.dzien;
+    cin >> osoba.dzienurodzenia.dzien;
     return osoba;
 }
 
 void drukujosobe(Osoba osoba)
 {
+    Data data = osoba.dzienurodzenia;
     cout << "imie i nazwisko: " << osoba.imie << " " << osoba.nazwisko;
-    cout << " urodzony: " << osoba.rok << "-" << osoba.mies << "-" << osoba.dzien;
+    cout << " urodzony: " << data.rok << "-" << data.mies << "-" << data.dzien;
 }
 
 int main()
 {
     Osoba tablica[10];
-    // Osoba osoba = wczytywanieosoby();
-    // drukujosobe(osoba);
     bool zapierdalaj = true;
     int licznik = 0;
     while (zapierdalaj)
     {
+        if (licznik >= 10) 
+        {
+            cout << "!NIE MOŻESZ KONTYNUOWAĆ!" << endl;
+            break;
+        }
 
         tablica[licznik] = wczytywanieosoby();
+        licznik++;
+
         char TczyN;
-        if (10 - licznik > 0)
+        if (licznik < 10)
         {
             cout << "Zostało " << 10 - licznik << " przebiegów. Czy dalej zapierdalac? (T - tak) (N - nie)  --> ";
             cin >> TczyN;
-        }
-        else
-        {
-            cout << "!NIE MOŻESZ KONTYNUOWAĆ!";
-            break;
-        }
-
-        licznik++;
-        if (TczyN == 'N')
-        {
-            break;
+            if (TczyN == 'N')
+            {
+                zapierdalaj = false;
+            }
         }
     }
+
     for (int i = 0; i < licznik; i++)
     {
         drukujosobe(tablica[i]);
         cout << endl;
     }
+
+    return 0;
 }
